d6085eb3fd6055312d50868f28b7414fd8d4ea93
Adriana Pope <adrianagiancarla@gmail.com>
2015-06-20 14:14:58 -0700

    added some more fields to display on the show file.

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -5,15 +5,29 @@
 <h1>{{ $recipe->name }}</h1>
 	
 	<article>
-		<h3>Description</h3>
+		<h2>Description</h2>
 		{{ $recipe->description }}
 
-		<h3>Instructions</h3>
+		<h2>Instructions</h2>
 		{{ $recipe->instructions }}
 
-		<h3>Notes</h3>
+		<h2>Notes</h2>
 		{{ $recipe->notes }}
-	</article>
 
+		<h2>Published On:</h2>
+		{{ $recipe->published_at }}
+
+		<h2>Prep Time:</h2>
+		{{ $recipe->prep_time}} {{ $recipe->prep_time_type }}
+
+		<h2>Cook Time:</h2>
+		{{ $recipe->cook_time }} {{ $recipe->cook_time_type }}
+
+		<h2>Ready Time:</h2>
+		{{ $recipe->ready_time }} {{ $recipe->ready_time_type }}
+
+		<h2>Servings:</h2>
+		{{ $recipe->servings }}
+	</article>
 
 @stop
