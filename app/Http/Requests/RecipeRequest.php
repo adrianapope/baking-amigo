9af52d59f16ffc0d5923e72e9073f2d2518ce206
Adriana Pope <adrianagiancarla@gmail.com>
2015-06-20 14:09:35 -0700

    renamed file to RecipeRequest so that it is a more general request and can handle both edit and create

--- app/Http/Requests/RecipeRequest.php
+++ app/Http/Requests/RecipeRequest.php
@@ -2,7 +2,7 @@
 
 use App\Http\Requests\Request;
 
-class CreateRecipeRequest extends Request {
+class RecipeRequest extends Request {
 
 	/**
 	 * Determine if the user is authorized to make this request.
