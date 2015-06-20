fd3d06d71412379134528284cc9e4ee2735097e7
Adriana Pope <adrianagiancarla@gmail.com>
2015-06-20 14:13:14 -0700

    1.) created a partial for the form and named it form.blade.php for all the form code 2.) created a variable called $submitButtonText that the view will need 3.) now both edit.blade.php and create.blade.php use @include (form.blade.php) and then i pass in the variable and can customize for editing vs creating a recipe

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -7,84 +7,10 @@
 
 	<div class="row">
 		{!! Form::open(['url' => 'recipes', 'class' => 'col-sm-6']) !!}
-
-			<div class="form-group">
-				{!! Form::label('name', 'Recipe Name:') !!}
-				{!! Form::text('name', null, ['class' => 'form-control']) !!}
-			</div>
-
-			<div class="form-group">
-				{!! Form::label('description', 'Description:') !!}
-				{!! Form::textarea('description', null, ['rows'=> '4', 'class' => 'form-control']) !!}
-			</div>
-
-			<div class="form-group">
-				{!! Form::label('instructions', 'Instructions:') !!}
-				{!! Form::textarea('instructions', null, ['rows'=> '4', 'class' => 'form-control']) !!}
-			</div>
-
-			<div class="form-group">
-				{!! Form::label('notes', 'Notes: (optional)') !!}
-				{!! Form::textarea('notes', null, ['rows'=> '4', 'class' => 'form-control']) !!}
-			</div>
-
-			<div class="form-group">
-				{!! Form::label('published_at', 'Publish On:') !!}
-				{!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-			</div>
-
-			<div class="row">
-				<div class="form-group col-sm-6">
-					{!! Form::label('prep_time', 'Prep Time:') !!}
-					{!! Form::input('prep_time', null, null, ['class' => 'form-control']) !!}
-				</div>
-				<div class="form-group col-sm-6">
-					{!! Form::label('prep_time_type', '&nbsp;') !!}
-					{!! Form::select('prep_time_type', ['minutes' => 'Minutes', 'hours' => 'Hours'], null, ['class' => 'form-control']) !!}
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="form-group col-sm-6">
-					{!! Form::label('cook_time', 'Cook Time:') !!}
-					{!! Form::input('cook_time', null, null, ['class' => 'form-control']) !!}
-				</div>
-				<div class="form-group col-sm-6">
-					{!! Form::label('cook_time_type', '&nbsp;') !!}
-					{!! Form::select('cook_time_type', ['minutes' => 'Minutes', 'hours' => 'Hours'], null, ['class' => 'form-control']) !!}
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="form-group col-sm-6">
-					{!! Form::label('ready_time', 'Ready Time:') !!}
-					{!! Form::input('ready_time', null, null, ['class' => 'form-control']) !!}
-				</div>
-				<div class="form-group col-sm-6">
-					{!! Form::label('ready_time_type', '&nbsp;') !!}
-					{!! Form::select('ready_time_type', ['minutes' => 'Minutes', 'hours' => 'Hours'], null, ['class' => 'form-control']) !!}
-				</div>
-			</div>
-
-			<div class="form-group">
-				{!! Form::label('servings', 'Servings:') !!}
-				{!! Form::input('servings', 'servings', null, ['placeholder' => 'example: 1 dozen', 'class' => 'form-control']) !!}
-			</div>
-
-
-			<div class="form-group">
-				{!! Form::submit('Submit Recipe', ['class' => 'btn btn-primary form-control']) !!}
-			</div>
-
+			@include ('recipes.form', ['submitButtonText' => 'Submit Recipe'])
 		{!! Form::close() !!}
 	</div>
 
-@if ($errors->any())
-	<ul class="alert alert-danger">
-		@foreach ($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
-@endif
+	@include ('errors.list')
 
 @stop
---
+++ resources/views/recipes/form.blade.php
@@ -0,0 +1,67 @@
+			<div class="form-group">
+				{!! Form::label('name', 'Recipe Name:') !!}
+				{!! Form::text('name', null, ['class' => 'form-control']) !!}
+			</div>
+
+			<div class="form-group">
+				{!! Form::label('description', 'Description:') !!}
+				{!! Form::textarea('description', null, ['rows'=> '4', 'class' => 'form-control']) !!}
+			</div>
+
+			<div class="form-group">
+				{!! Form::label('instructions', 'Instructions:') !!}
+				{!! Form::textarea('instructions', null, ['rows'=> '4', 'class' => 'form-control']) !!}
+			</div>
+
+			<div class="form-group">
+				{!! Form::label('notes', 'Notes: (optional)') !!}
+				{!! Form::textarea('notes', null, ['rows'=> '4', 'class' => 'form-control']) !!}
+			</div>
+
+			<div class="form-group">
+				{!! Form::label('published_at', 'Publish On:') !!}
+				{!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
+			</div>
+
+			<div class="row">
+				<div class="form-group col-sm-6">
+					{!! Form::label('prep_time', 'Prep Time:') !!}
+					{!! Form::input('prep_time', null, null, ['class' => 'form-control']) !!}
+				</div>
+				<div class="form-group col-sm-6">
+					{!! Form::label('prep_time_type', '&nbsp;') !!}
+					{!! Form::select('prep_time_type', ['minutes' => 'Minutes', 'hours' => 'Hours'], null, ['class' => 'form-control']) !!}
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="form-group col-sm-6">
+					{!! Form::label('cook_time', 'Cook Time:') !!}
+					{!! Form::input('cook_time', null, null, ['class' => 'form-control']) !!}
+				</div>
+				<div class="form-group col-sm-6">
+					{!! Form::label('cook_time_type', '&nbsp;') !!}
+					{!! Form::select('cook_time_type', ['minutes' => 'Minutes', 'hours' => 'Hours'], null, ['class' => 'form-control']) !!}
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="form-group col-sm-6">
+					{!! Form::label('ready_time', 'Ready Time:') !!}
+					{!! Form::input('ready_time', null, null, ['class' => 'form-control']) !!}
+				</div>
+				<div class="form-group col-sm-6">
+					{!! Form::label('ready_time_type', '&nbsp;') !!}
+					{!! Form::select('ready_time_type', ['minutes' => 'Minutes', 'hours' => 'Hours'], null, ['class' => 'form-control']) !!}
+				</div>
+			</div>
+
+			<div class="form-group">
+				{!! Form::label('servings', 'Servings:') !!}
+				{!! Form::input('servings', 'servings', null, ['placeholder' => 'example: 1 dozen', 'class' => 'form-control']) !!}
+			</div>
+
+
+			<div class="form-group">
+				{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
+			</div>
