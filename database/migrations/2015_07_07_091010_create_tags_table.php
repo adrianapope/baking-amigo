0f35baca063b93ddda81453fa355c58cb66d716d
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-08 11:53:26 +0700

    creating a schema for tags and recipe_tag so that there can be a relationship between the recipe_id and the tag_id

---
+++ database/migrations/2015_07_07_091010_create_tags_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+//singular and alphabetical order
+        Schema::create('recipe_tag', function (Blueprint $table) {
+            $table->integer('recipe_id')->unsigned()->index();
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+
+            $table->integer('tag_id')->unsigned()->index();
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+        });
+     }
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tags');
+
+        Schema::drop('recipe_tag');
+	}
+
+}
