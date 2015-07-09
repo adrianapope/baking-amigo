ede81e76e1643047904b6c97e1897e0dfa89d5eb
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-09 18:33:24 +0700

    jqery plugin references and stylesheet references. also did an elixir function to reference the style sheets that are dynamic so they change when the files are changed (references the manifest file)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,31 +4,34 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+{{--<link rel="stylesheet" href="/css/app.css"> --}}
+	<link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css">
 </head>
 
 <body>
 	@include('layouts.nav')
 	<div class="container">
-{{-- #1 way of doing this. --}}
+		{{-- #1 way of doing this. --}}
         @include('partials.flash')
 
 
- {{--#2 notice it is :: and not just a single dot. use this with the laracasts/flash package
-
+ 		{{--#2 notice it is :: and not just a single dot. use this with the laracasts/flash package
         @include('flash::message')--}}
 
 
 		@yield('content')
 	</div>
 
-	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
    <script>
 
-/* #3 way of doing this. doing an overlay.
+		/* #3 way of doing this. doing an overlay.
        $('flash-overlay-modal').modal();
-*/
+		*/
 
        $('div.alert').not('.alert-important').delay(3000).slideUp(300);
    </script>
