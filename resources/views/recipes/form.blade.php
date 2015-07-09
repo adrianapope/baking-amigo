2e4af8c8fff4949df3267a3dd8cf24bfd090fd9e
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-09 18:34:29 +0700

    added an 'id' => 'tag_list' also added a script so that there will be a placeholder and also it will reference the tag_list

--- resources/views/recipes/form.blade.php
+++ resources/views/recipes/form.blade.php
@@ -65,9 +65,18 @@
 			<!-- Tags Form Input -->
             <div class-="form-group">
                 {!! Form::label('tag_list', 'Tags:') !!}
-                {!! Form::select('tag_list[]', $tags, null, ['class' => 'form-control', 'multiple']) !!}
+                {!! Form::select('tag_list[]', $tags, null, ['id' => 'tag_list', 'class' => 'form-control', 'multiple']) !!}
             </div>
 
 			<div class="form-group">
 				{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
 			</div>
+
+			@section('footer')
+				<script>
+					$('#tag_list').select2({
+						placeholder: 'Choose a tag',
+						//tags: true,
+					});
+				</script>
+			@endsection
