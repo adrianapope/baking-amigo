5d4c8217f5d53e6b2393e9b3bf4a992ede08faf7
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-10 15:13:01 +0700

    added ViewComposerServiceProvider to the service providers section

--- config/app.php
+++ config/app.php
@@ -145,8 +145,10 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
+        'App\Providers\ViewComposerServiceProvider',
 
         'Laracasts\Flash\FlashServiceProvider'
+
 
 	],
 
