eaea085eefac831b13171b4d8baa11d7272a06e0
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-15 12:50:30 +0700

    created a WelcomeController with code for how to fetch configuration (just as an example) through the controller one way is config facade. another is referencing the actual concrete class another is using an interface. another way is ('config')

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,25 +1,45 @@
 <?php namespace App\Http\Controllers;
 
-class WelcomeController extends Controller {
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use Illuminate\Contracts\Config\Repository;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Welcome Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders the "marketing page" for the application and
-	| is configured to only allow guests. Like most of the other sample
-	| controllers, you are free to modify or remove it as you desire.
-	|
-	*/
+class WelcomeController extends Controller
+{
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
+	protected $config;
+
+	/*public function __construct()
 	{
 		$this->middleware('guest');
+	}*/
+
+	public function __construct(Repository $config)
+	{
+
+		$this->config = $config;
+
+	}
+
+	public function test()
+	{
+		// constructor injection
+		return $this->config->get('database.default');
+		// method injection
+		return $config->get('database.defualt');  // laravel five supports method injection on controllers. just pass through Repository $config into the public function test() method
+		// facade
+		// config helper function
 	}
 }
+
+// how to reference this stuff in your controller
+// we just want to fetch our default database type  (just as an example)
+// with the first two, you have the option of referencing the concrete implementation
+// or the contract/interface itself
+// in the construct, we will typehint the config/Repository
+// and import this up top
+// you can reference the concrete implemnetation (Illuminate\Config\Repository) or the abstract/interface/contract (Illuminate\Contracts\Config\Repository) and that's what we'll do in thi s case
+// the __construct that deals with $config takes care of the constructor injection option
+// we would just say return this config get database.default and in chrome we get mysql
+// so that is constructor injection using an interface
+// how did laravel know that for this interface it should use some specific implementation? next episode.
