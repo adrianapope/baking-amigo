daa6a4109b038a0bca3d550c81609e26d346347b
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-16 17:25:08 +0700

    changed text about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -16,6 +16,6 @@
 
 
 <p>
-	Hi. I built this recipe aggregator because I absolutely love baking and coding so I've merged my two passions together in one place. Thanks in advance for sharing your baking recipes!
+	Hi, and thank you for visiting Baking Amigo! When I'm not busy writing code, I really enjoy baking no matter what time of year it is. I built this recipe aggregator as a way to merge my two passions together into one convenient place. I hope that you'll decide to share your awesome baking recipes with our little community. -Adriana Pope
 	</p>
 @stop
