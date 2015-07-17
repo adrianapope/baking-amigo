1e01a42c2f574354204bca0d6b9ca2e617f3a34a
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-17 16:36:41 +0700

    changed from '$recipe->title' to '$recipe->name'

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-	<h1>Recipes</h1>	
+	<h1>Recipes</h1>
 
 	@foreach ($recipes as $recipe)
 		<article>
 			<h2>
-				<a href="/recipes/{{ $recipe->id }}">{{ $recipe->name }}</a> 			
+				<a href="/recipes/{{ $recipe->id }}">{{ $recipe->name }}</a>
 			</h2>
 
 			<div class="body">{{ $recipe->description }}</div>
