41ea606823522fcad28ad8a6d5e934dbc0053fe3
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-17 14:03:43 +0700

    made navigation buttons pills

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,10 +11,10 @@
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/recipes">Recipes</a></li>
+      <ul class="nav nav-pills">
+        <li role="presentation" class="active"><a href="/about">About</a></li>
+        <li role="presentation"><a href="/contact">Contact</a></li>
+        <li role="presentation"><a href="/recipes">Recipes</a></li>
       </ul>
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
