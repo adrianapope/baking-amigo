6f0989611677a0840fffbc3dd67dea85e8e4217e
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-17 16:31:46 +0700

    gulp related changes (so that css files will talk to each other) changed to app.css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,16 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
- 	<link rel="stylesheet" href="/css/app.css">
+ 	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css">
-	<link rel="stylesheet" href="{{ elixir('css/all.css') }}">
  	</head>
 <body>
 	@include('layouts.nav')
 	<div class="container">
         @include('partials.flash')
-
 		@yield('content')
 	</div>
 
@@ -20,6 +17,6 @@
 
 	<script src="js/all.js"></script>
 
-		@yield('footer')
+	@yield('footer')
 </body>
 </html>
