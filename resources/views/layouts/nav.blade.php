19e9b376d83daead26a1e9caa48a1dc7ae2724ec
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-17 14:25:23 +0700

    deleted link and dropdown menu and placed the signup/login buttons over there. made buttons default so no color for now

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,14 +10,11 @@
       <a class="navbar-brand" href="/">Baking Amigo</a>
     </div>
 
-
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
           <li><a href="/recipes">Recipes</a></li>
-          <li role="presentation"><a href="/auth/register" class="btn btn-primary" role="button">Sign Up!</a>
-          <li role="presentation"><a href="/auth/login" class="btn btn-primary" role="button">Login</a>
       </ul>
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
@@ -25,18 +22,8 @@
         </div>
       </form>
       <ul class="nav navbar-nav navbar-right">
-       {{--  <li>{!! link_to_action('RecipesController@show', $latest->name, [$latest->id]) !!}</li> --}}
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
+        <li role="presentation"><a href="/auth/register" class="btn btn-default" role="button">Sign Up!</a>
+        <li role="presentation"><a href="/auth/login" class="btn btn-default" role="button">Login</a>
       </ul>
     </div>
   </div>
