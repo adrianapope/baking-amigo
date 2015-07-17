223b5d0662a77c2ad3a0ddeef4131376aec670d8
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-17 16:33:25 +0700

    deleted some dynamic code & updated text

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,19 +3,7 @@
 @section('content')
 
 <h1>About</h1>
-
-@if (count($recipes))
-	<h3>My favorite baking recipes include:</h3>
-	<ul>
-		@foreach ($recipes as $recipe)
-		<li><a href="/recipes/{{ $recipe->id }}">{{ $recipe->title }}</a></li>
-		@endforeach
-	</ul>
-@endif
-
-
-
-<p>
-	Hi, and thank you for visiting Baking Amigo! When I'm not busy writing code, I really enjoy baking no matter what time of year it is. I built this recipe aggregator as a way to merge my two passions together into one convenient place. I hope that you'll decide to share your awesome baking recipes with our little community. -Adriana Pope
+	<p>
+	Hi there and thank you for visiting Baking Amigo! When I'm not busy writing code, I really enjoy baking no matter what time of year it is. I built this recipe aggregator as a way to merge my two passions together into one convenient place. I hope that you'll decide to share your awesome baking recipes with our little community.
 	</p>
 @stop
