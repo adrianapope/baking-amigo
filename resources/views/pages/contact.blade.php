25224f0160e87510f70439d6727c2a20b61e6af6
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-17 16:33:46 +0700

    deleted script and updated text

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-	<h1>contact me!</h1>
+<h1>Contact</h1>
+	<p>You can reach out to us at bakingamigo&#64;gmail.com</p>
 @stop
 
 @section('footer')
-	<script>alert('Contact form scripts');</script>
 @stop
