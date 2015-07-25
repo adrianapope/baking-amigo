b2e53f646f341a19ddf55593ae35e68ede4cd319
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-25 19:56:57 +0700

    hunk 1: changed from lead to text-info hunk 2: added classes to my little circle headlines hunk 3: took out some old code where the placeholder images were bigger. now they are smaller 250x250

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
      <!-- Jumbotron -->
+
       <div class="text-center">
         <div class="jumbotron">
           <h1>Find & Swap Recipes.</h1>
-          <p class="lead">!Hola! Welcome to our little community of fellow baking enthusiasts where everyone is an amigo. Whether you are just a newb trying out your mitts, or an experienced macaroon expert, we'd love to share your recipes with the rest of the world. Híjole, what are you waiting for?</p>
+          <p class="text-info">!Hola! Welcome to our little community of fellow baking enthusiasts where everyone is an amigo. Whether you are just a newb trying out your mitts, or an experienced macaroon expert, we'd love to share your recipes with the rest of the world. Híjole, what are you waiting for?</p>
           <p><a class="btn btn-lg btn-success" href="/recipes/create" role="button">Submit a Recipe Today</a>
           </p>
-      </div>
+      	</div>
       </div>
 
 
@@ -19,18 +20,18 @@
 			<div class="row">
 				<div class="col-lg-4">
 					<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-					<h2>Become an Amigo!</h2>
-					<p>Want to bake something amazing that impresses heaps of folks? Join today for free.</p>
+					<h2 class="homepage-heading">Become an Amigo!</h2>
+					<p class="text-muted">Want to bake something amazing that impresses heaps of folks? Join today for free.</p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-lg-4">
 					<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-					<h2>Ándale, Level Up</h2>
-					<p>Learn tips and tricks from fellow bakers. Your next challenge awaits you.  </p>
+					<h2 class="homepage-heading">Ándale, Level Up</h2>
+					<p class="text-muted">Learn tips and tricks from fellow bakers. Your next challenge awaits you.  </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-lg-4">
 					<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-					<h2>¡Órale! Sharing is Caring</h2>
-					<p>Write a recipe, then use a recipe. It's the recipe cycle that keeps on giving.</p>
+					<h2 class="homepage-heading">¡Órale! Sharing is Caring</h2>
+					<p class="text-muted">Write a recipe, then use a recipe. It's the recipe cycle that keeps on giving.</p>
 				</div><!-- /col-lg-4 -->
 			</div><!-- /.row -->
 		</div>
@@ -39,76 +40,60 @@
 
       <!-- START RECIPES -->
 
-
-{{--     <div class="homepage-recipes-wrapper">
-    	<div class="container">
-	      <div class="row">
-	        <div class="col-sm-3">
-	          <img class="img-square img-responsive" src="http://dummyimage.com/300x300/e693e6/1c29db.jpg" alt="Generic placeholder image" width="300" height="300">
-	        </div>
-	        <div class="col-sm-3">
-	          <img class="img-square img-responsive" src="http://dummyimage.com/300x300/e693e6/1c29db.jpg" alt="Generic placeholder image" width="300" height="300">
-	        </div>
-	        <div class="col-sm-3">
-	          <img class="img-square img-responsive" src="http://dummyimage.com/300x300/e693e6/1c29db.jpg" alt="Generic placeholder image" width="300" height="300">
-	        </div>
-	        <div class="col-sm-3">
-	          <img class="img-square img-responsive" src="http://dummyimage.com/300x300/e693e6/1c29db.jpg" alt="Generic placeholder image" width="300" height="300">
-	        </div>
-	      </div>
-	    </div>
-	</div>
- --}}
-	<div class="homepage-recipes-wrapper-first">
+	<div class="homepage-recipes-wrapper">
 		<div class="container">
 	      <div class="row">
 	        <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				{{-- $img = Image::make(Input::file('img/cookies-flower-lemon.jpg'))
+				$img->resize(250, 250);
+				return Response::make($image, 200, ['Content-Type' => 'image/jpg']);
+				}); --}}
+				<img src="img/cookies-flower-lemon.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 				</div>
 			</div>
 		</div>
 	</div>
-		<div class="homepage-recipes-wrapper-second">
+		<div class="homepage-recipes-wrapper">
 		<div class="container">
 	      <div class="row">
 	        <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 				</div>
 			</div>
 		</div>
 	</div>
-		<div class="homepage-recipes-wrapper-third">
+		<div class="homepage-recipes-wrapper">
 		<div class="container">
 	      <div class="row">
 	        <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded">
+				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 				</div>
 			</div>
 		</div>
