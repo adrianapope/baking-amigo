e0d0a7c8c008614ad1cf678530b958744bac1031
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 14:15:26 +0700

    changed text and photo on about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,11 +5,17 @@
 <div class="about-wrapper">
 	<div class="container">
 		<h1>About</h1>
-		<blockquote>
-		<p>"Baking Amigo is a space where folks can find and swap thier favorite allergen-free baking recipes."</p>
-		</blockquote>
-		<p>If you have ever found yourself drooling over a slice of cake in a display window, but weren't able to eat it because you are allergic to either gluten, sugar, dairy (or whatever ingredient it may be) ... we know exactly how you feel! Baking Amigo understands how hard it is to satisfy your sweet tooth when you have a specific allergyor food sensitivity. This website was created as a resource for those looking for an awesome dessert recipe without such and such ingredient.</p>
-		<img src="img/small-cupcakes.jpg" alt="Cupcakes" class="img-rounded img-responsive width="200" height="200"">
+		<div class="body">
+			<blockquote>
+				<p>"Baking Amigo is a space where folks can find and swap thier favorite allergen-free baking recipes."
+				</p>
+			</blockquote>
+			<p>If you have ever found yourself drooling over a slice of cake or some delicious cookies in a display window, but weren't able to eat it because you are allergic to either gluten, sugar, dairy (or whatever ingredient it may be) ... we know exactly how you feel! Baking Amigo understands how hard it is to satisfy your sweet tooth when you have a specific allergy or food sensitivity. This website was created as a resource for those looking for an awesome dessert recipe without any unwanted ingredients.
+			</p>
+			<div class="post_about_text">
+				<img src="img/chocolate-chip-cookies.jpg" alt="Cupcakes" class="img-rounded img-responsive">
+			</div>
+		</div>
 	</div>
 </div>
 @stop
