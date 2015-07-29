9d4d6dabc5cb8e263022b4e7ce6097458b1dee32
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 18:48:19 +0700

    cleaned up div organization. added a button for editing your profile if the profile belongs to you.

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,5 +1,14 @@
 @extends('app')
 
 @section('content')
-	<h1>Profile</h1>
+	<div class="container">
+		<div class="profiles-index">
+			<h1>Profile</h1>
+				<div class="page-navigation-wrapper">
+					@if( Auth::user()->id == $user->id)
+						<a href="#"><span class="btn btn-success">Edit Profile</span></a>
+					@endif
+				</div>
+		</div>
+	</div>
 @stop
