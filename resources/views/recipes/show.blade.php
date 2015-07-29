9a3ea5ac3b0f13ca10b34d627db52295f91cdfd5
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 14:11:02 +0700

    added a div at top for designing purposes

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -9,41 +9,45 @@
 
 @section('content')
 
-<h1>{{ $recipe->name }}</h1>
+<div class="show-wrapper">
+	<div class="container">
+		<h1>{{ $recipe->name }}</h1>
 
-	<article>
-		<h2>Description</h2>
-		{{ $recipe->description }}
+			<article>
+				<h2>Description</h2>
+				{{ $recipe->description }}
 
-		<h2>Instructions</h2>
-		{{ $recipe->instructions }}
+				<h2>Instructions</h2>
+				{{ $recipe->instructions }}
 
-		<h2>Notes</h2>
-		{{ $recipe->notes }}
+				<h2>Notes</h2>
+				{{ $recipe->notes }}
 
-		<h2>Published On:</h2>
-		{{ $recipe->published_at }}
+				<h2>Published On:</h2>
+				{{ $recipe->published_at }}
 
-		<h2>Prep Time:</h2>
-		{{ $recipe->prep_time}} {{ $recipe->prep_time_type }}
+				<h2>Prep Time:</h2>
+				{{ $recipe->prep_time}} {{ $recipe->prep_time_type }}
 
-		<h2>Cook Time:</h2>
-		{{ $recipe->cook_time }} {{ $recipe->cook_time_type }}
+				<h2>Cook Time:</h2>
+				{{ $recipe->cook_time }} {{ $recipe->cook_time_type }}
 
-		<h2>Ready Time:</h2>
-		{{ $recipe->ready_time }} {{ $recipe->ready_time_type }}
+				<h2>Ready Time:</h2>
+				{{ $recipe->ready_time }} {{ $recipe->ready_time_type }}
 
-		<h2>Servings:</h2>
-		{{ $recipe->servings }}
+				<h2>Servings:</h2>
+				{{ $recipe->servings }}
 
-		@unless ($recipe->tags->isEmpty())
-		<h5>Tags:</h5>
-		<ul>
-			@foreach ($recipe->tags as $tag)
-				<li>{{ $tag->name }}</li>
-			@endforeach
-		</ul>
-		@endunless
-	</article>
+				@unless ($recipe->tags->isEmpty())
+				<h5>Tags:</h5>
+				<ul>
+					@foreach ($recipe->tags as $tag)
+						<li>{{ $tag->name }}</li>
+					@endforeach
+				</ul>
+				@endunless
+			</article>
+	</div>
+</div>
 
 @stop
