0af1fedb204e7c16d4d1ff9d2a8bdbfbcf040c45
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 14:14:35 +0700

    added a div at top for designing purposes. made email clickable and opens up an email with a subject line.

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,10 +2,20 @@
 
 @section('content')
 
-<h1>Contact</h1>
-	<p>You can reach out to us at bakingamigo&#64;gmail.com</p>
+<div class="contact-wrapper">
+	<div class="container">
+		<h1>Contact</h1>
+		<div class="body">
+			<p>Have a question or suggestion about our site? Feel free to email us at <a href="mailto:bakingamigo@gmail.com?subject=Contact Us: Baking Amigo Question/Suggestion">bakingamigo&#64;gmail.com</a> and we'll make sure to get in touch with you as soon as we can.
+			</p>
+			<div class="post_contact_text">
+				<img src="img/small-cupcakes.jpg" alt="Contact Us Adriana Pope" class="img-rounded img-responsive">
+			</div>
+		</div>
+	</div>
+</div>
 @stop
-
 
 @section('footer')
 @stop
+
