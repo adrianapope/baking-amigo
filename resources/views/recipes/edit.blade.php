9b9dc3a752bcc7b0fcc4cac734573e6fda2156db
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 18:47:29 +0700

    cleaned up div organization

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -1,15 +1,17 @@
 @extends('app')
 
 @section('content')
-	<h1>Edit: {{ $recipe->name }}</h1>
-
-	<div class="row">
-		{!! Form::model($recipe, ['method' => 'PATCH', 'action' => ['RecipesController@update', $recipe->id]]) !!}
-			@include ('recipes.form', ['submitButtonText' => 'Update Recipe'])
-		{!! Form::close() !!}
+	<div class="container">
+		<div class="edit-index">
+			<h1>Edit: {{ $recipe->name }}</h1>
+				<div class="edit-index-wrapper">
+				{!! Form::model($recipe, ['method' => 'PATCH', 'action' => ['RecipesController@update', $recipe->id]]) !!}
+					@include ('recipes.form', ['submitButtonText' => 'Update Recipe'])
+				{!! Form::close() !!}
+				</div>
+		</div>
 	</div>
-
+	<div class="errors-wrapper">
 	@include ('errors.list')
-
-
+	</div>
 @stop
