bf0cc2169012105d6ce5f6da68c3f7e60d327945
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 14:12:17 +0700

    added a div at top for designing purposes and changed button "create a recipe" so color can be customizable. made it a large button too.

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,19 +1,24 @@
+
+
 @extends('app')
 
 @section('content')
 
-	<h1>Recipes</h1>
+<div class="index-wrapper">
+	<div class="container">
+		<h1>Recipes</h1>
 
-	@foreach ($recipes as $recipe)
-		<article>
-			<h2>
-				<a href="/recipes/{{ $recipe->id }}">{{ $recipe->name }}</a>
-			</h2>
+		@foreach ($recipes as $recipe)
+			<article>
+				<h2>
+					<a href="/recipes/{{ $recipe->id }}">{{ $recipe->name }}</a>
+				</h2>
 
-			<div class="body">{{ $recipe->description }}</div>
-		</article>
-	@endforeach
+				<div class="body">{{ $recipe->description }}</div>
+			</article>
+		@endforeach
 
-	<a href="/recipes/create"><span class="btn btn-primary">Create a Recipe</span></a>
-
+		<a href="/recipes/create"><span class="btn btn-default btn-lg">Create a Recipe</span></a>
+	</div>
+</div>
 @stop
