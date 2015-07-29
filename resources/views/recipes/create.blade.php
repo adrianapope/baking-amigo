9f139694202189a7ce4eae6874e5211fdb9f21a4
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 18:47:50 +0700

    cleaned up div organization

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -1,16 +1,18 @@
 @extends('app')
 
 @section('content')
-	<h1>Write a New Recipe</h1>
-
-	<hr/>
-
-	<div class="row">
-		{!! Form::model($recipe = new \App\Recipe, ['url' => 'recipes', 'class' => 'col-sm-6']) !!}
-			@include ('recipes.form', ['submitButtonText' => 'Submit Recipe'])
-		{!! Form::close() !!}
+	<div class="container">
+		<div class="create-index">
+			<h1>Write a New Recipe</h1>
+			<hr/>
+				<div class="create-index-wrapper">
+					{!! Form::model($recipe = new \App\Recipe, ['url' => 'recipes', 'class' => 'col-sm-6']) !!}
+						@include ('recipes.form', ['submitButtonText' => 'Submit Recipe'])
+					{!! Form::close() !!}
+				</div>
+		</div>
 	</div>
-
+	<div class="errors-wrapper">
 	@include ('errors.list')
-
+	</div>
 @stop
