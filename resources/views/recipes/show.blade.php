7640d9b9ea5f905a2048d360d6c7e588535cb882
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-29 18:45:34 +0700

    cleaned up div organization. also added button so that if it is your recipe that you created, then you can edit it.

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -8,46 +8,49 @@
 @endsection
 
 @section('content')
-
-<div class="show-wrapper">
 	<div class="container">
-		<h1>{{ $recipe->name }}</h1>
+		<div class="show-index">
+			<h1>{{ $recipe->name }}</h1>
 
-			<article>
-				<h2>Description</h2>
-				{{ $recipe->description }}
+				<div class="show-index-wrapper">
+					<h2>Description</h2>
+						{{ $recipe->description }}
 
-				<h2>Instructions</h2>
-				{{ $recipe->instructions }}
+						<h2>Instructions</h2>
+						{{ $recipe->instructions }}
 
-				<h2>Notes</h2>
-				{{ $recipe->notes }}
+						<h2>Notes</h2>
+						{{ $recipe->notes }}
 
-				<h2>Published On:</h2>
-				{{ $recipe->published_at }}
+						<h2>Published On:</h2>
+						{{ $recipe->published_at }}
 
-				<h2>Prep Time:</h2>
-				{{ $recipe->prep_time}} {{ $recipe->prep_time_type }}
+						<h2>Prep Time:</h2>
+						{{ $recipe->prep_time}} {{ $recipe->prep_time_type }}
 
-				<h2>Cook Time:</h2>
-				{{ $recipe->cook_time }} {{ $recipe->cook_time_type }}
+						<h2>Cook Time:</h2>
+						{{ $recipe->cook_time }} {{ $recipe->cook_time_type }}
 
-				<h2>Ready Time:</h2>
-				{{ $recipe->ready_time }} {{ $recipe->ready_time_type }}
+						<h2>Ready Time:</h2>
+						{{ $recipe->ready_time }} {{ $recipe->ready_time_type }}
 
-				<h2>Servings:</h2>
-				{{ $recipe->servings }}
+						<h2>Servings:</h2>
+						{{ $recipe->servings }}
 
-				@unless ($recipe->tags->isEmpty())
-				<h5>Tags:</h5>
-				<ul>
-					@foreach ($recipe->tags as $tag)
-						<li>{{ $tag->name }}</li>
-					@endforeach
-				</ul>
-				@endunless
-			</article>
+						@unless ($recipe->tags->isEmpty())
+						<h5>Tags:</h5>
+						<ul>
+							@foreach ($recipe->tags as $tag)
+								<li>{{ $tag->name }}</li>
+							@endforeach
+						</ul>
+						@endunless
+				</div>
+		</div>
 	</div>
-</div>
-
+	<div class="page-navigation-wrapper">
+		@if( Auth::user()->id == $recipe->user_id)
+			<a href="/recipes/{{ $recipe->id }}/edit"><span class="btn btn-success">Edit your Recipe</span></a>
+		@endif
+	</div>
 @stop
