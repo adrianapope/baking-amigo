29f56a7c47e04ee741ca1fd3113e35b63b210759
Adriana Pope <adrianagiancarla@gmail.com>
2015-07-30 16:29:34 +0700

    Added a more complete view of the user's profile showing their name, bio, location, and twitter/instagram handle names

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -3,12 +3,22 @@
 @section('content')
 	<div class="container">
 		<div class="profiles-index">
-			<h1>Profile</h1>
-				<div class="page-navigation-wrapper">
-					@if( Auth::user()->id == $user->id)
-						<a href="#"><span class="btn btn-success">Edit Profile</span></a>
-					@endif
-				</div>
+			<h2>{{ $user->name }} <small> {{ $user->profile->location }}</small></h2>
+			<div class="bio">
+				<p>
+					{{ $user->profile->bio }}
+				</p>
+			</div>
+			<ul class="links">
+				<li>{!! link_to('http://twitter.com/' . $user->profile->twitter_username, 'Find Me On Twitter') !!}</li>
+				<li>{!! link_to('http://instagram.com/' . $user->profile->instagram_username, 'View My Pics On Instagram') !!}</li>
+			</ul>
+
+			<div class="page-navigation-wrapper">
+				@if( Auth::user() && Auth::user()->id == $user->id)
+					<a href="#"><span class="btn btn-success">Edit Your Profile</span></a>
+				@endif
+			</div>
 		</div>
 	</div>
 @stop
