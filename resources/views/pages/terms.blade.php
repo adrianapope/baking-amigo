909e2fa10ac79ec0c7177541437768f846cdb3c3
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-03 22:19:15 +0800

    added lore opossum

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -1,5 +1,17 @@
 @extends('app')
 
 @section('content')
-	<h1>Terms Page</h1>
+	<h1>Terms of Service</h1>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vestibulum neque ut ex consectetur, sed pretium dui ullamcorper. Morbi pretium bibendum lorem eget rutrum. Curabitur ut nisl pretium, viverra elit ut, malesuada mauris. Suspendisse luctus augue quis neque laoreet pellentesque. Proin id metus non velit facilisis consequat nec nec ligula. Proin et sodales odio, tincidunt ornare risus. Maecenas porttitor convallis ornare. Vivamus blandit laoreet porta.
+	</p>
+	<p>
+	Fusce tincidunt turpis eros, in accumsan est venenatis a. Integer tincidunt lacus sit amet lobortis dignissim. Phasellus mollis ultricies tellus. Aenean tincidunt condimentum lacus, a consectetur elit pellentesque finibus. Proin id nulla et leo posuere tempus. Suspendisse potenti. Vestibulum quis euismod risus, et interdum lacus. Cras consequat sit amet massa vitae faucibus. Donec in justo luctus, vulputate nisl a, rutrum felis. Etiam sodales sed dolor a interdum. Donec porta, eros nec finibus convallis, dui metus condimentum elit, vel aliquam purus nisi a ipsum.
+	</p>
+	<p>
+	Donec mauris sem, sagittis ut ipsum ut, tristique egestas elit. Donec eros lorem, scelerisque eget commodo at, mattis vel est. Quisque justo elit, finibus et ex nec, bibendum dictum erat. Sed pulvinar turpis eros, id feugiat nisl sollicitudin sed. Maecenas lacinia massa eros, non faucibus leo porttitor eu. Quisque pulvinar augue sed orci porta, ac consequat metus dapibus. Integer sit amet metus quis diam lobortis condimentum id non massa. Proin pellentesque iaculis fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis nec est maximus, pellentesque lorem ac, egestas ipsum. Maecenas vel ex eros. Praesent ut suscipit ante, ac congue neque.
+	</p>
+	<p>
+	Sed porta elit non massa tincidunt pulvinar. Maecenas rutrum id leo tincidunt convallis. Nulla mollis nec urna vitae facilisis. Aenean pellentesque, tellus sit amet efficitur facilisis, neque sem elementum lectus, id elementum odio erat et lacus. Mauris sed arcu accumsan, aliquet nisl a, feugiat dui. Nullam iaculis diam felis, in porta massa laoreet quis. Cras gravida, ipsum ut molestie eleifend, massa ante tempor ex, quis cursus orci ipsum et augue. Mauris pharetra sodales interdum. Mauris at tellus suscipit, condimentum est vel, vulputate nibh. Nam auctor, ante vitae malesuada vestibulum, sem magna sodales orci, vitae ultrices nibh nisi condimentum libero. Vestibulum pharetra, est id imperdiet facilisis, nunc lacus vulputate odio, tristique sodales tellus magna quis massa. Etiam felis ex, egestas nec pharetra ac, lacinia vel lectus. Aenean cursus vel tellus et placerat. Pellentesque pulvinar porttitor varius. Suspendisse vel elementum lacus.
+	</p>
 @stop
