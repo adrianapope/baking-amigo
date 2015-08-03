87dbba0938809dfc1a2a310967f4af897781e4ed
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-03 19:19:36 +0800

    styled footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,46 @@
 <footer class="footer">
 	<div class="container">
-		<p class="text-muted">&copy; 2015 Baking Amigo</p>
+		<div class="main-footer-wrapper">
+			<div class="row">
+				{{-- Nested divs begin here--}}
+				<div class="col-md-2 col-md-offset-4">
+					<h4>Company</h4>
+					<ul class="footer-links">
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<h4>Discover</h4>
+					<ul class="footer-links">
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<h4>Share</h4>
+						<ul class="footer-links">
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+						</ul>
+				</div>
+			</div>
+			<hr>
+
+			<div class="social-links-wrapper row">
+				<div class="col-md-12">
+					<h5>Join us on</h5>
+						<ul class="social-links">
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+						</ul>
+					<p class="text-muted">&copy; 2015 Baking Amigo</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
