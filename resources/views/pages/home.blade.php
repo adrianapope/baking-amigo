5cffd07d5f30cb7f709e56a76f5e11a7389070f9
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-04 20:54:37 +0800

    added images of cookies for the recipe-wrapper-points

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -29,19 +29,19 @@
     	<div class="container">
 			<div class="row">
 				<div class="col-lg-4">
-					<a href="auth/register"><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"></a>
+					<a href="auth/register"><img class="img-circle" src="img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
 					<a href="auth/register"><h2 class="homepage-heading">Join</h2></a>
 					<p class="text-muted">Want to bake something amazing that <br>
 					impresses heaps of folks? Sign up today. </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-lg-4">
-					<a href="recipes"><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"></a>
+					<a href="recipes"><img class="img-circle" src="img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
 					<a href="recipes"><h2 class="homepage-heading">Discover</h2></a>
 					<p class="text-muted">Learn tips and tricks from fellow <br>
 					bakers. Your next challenge awaits you. </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-lg-4">
-					<a href="recipes/create"><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"></a>
+					<a href="recipes/create"><img class="img-circle" src="img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
 					<a href="recipes/create"><h2 class="homepage-heading">Share</h2></a>
 					<p class="text-muted">Write a recipe, then use a recipe. <br>
 					It's the cycle that keeps on giving. </p>
@@ -57,19 +57,16 @@
 		<div class="container">
 	      <div class="row">
 	        <div class="col-sm-3">
-				{{-- $img = Image::make('img/cookies-flower-lemon.jpg')->resize(250, 250);
-				 return $img->response('jpg');
-				}); --}}
-				<img src="img/cookies-flower-lemon.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
+				<img src="img/tiny-cookie.jpg" alt="Cookie" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
+				<img src="img/tiny-bread.jpg" alt="Bread" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
+				<img src="img/tiny-cupcakes.jpg" alt="Cupcakes" class="img-rounded img-responsive">
 			</div>
 			  <div class="col-sm-3">
-				<img src="http://dummyimage.com/250x250/b3b1b3/dcdef5.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
+				<img src="img/tiny-handpies.jpg" alt="Handpies" class="img-rounded img-responsive">
 				</div>
 			</div>
 		</div>
