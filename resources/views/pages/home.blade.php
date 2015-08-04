a0857d446076f275d2fb8723e21df0d1ccae7a6b
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-04 12:47:41 +0800

    made recipe-wrapper-points clickable to various pages added pointer hover effect

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -19,20 +19,20 @@
     	<div class="container">
 			<div class="row">
 				<div class="col-lg-4">
-					<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-					<h2 class="homepage-heading">Become an Amigo</h2>
+					<a href="auth/register"><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"></a>
+					<a href="auth/register"><h2 class="homepage-heading">Join</h2></a>
 					<p class="text-muted">Want to bake something amazing that <br>
-					impresses heaps of folks? Join today. </p>
+					impresses heaps of folks? Sign up today. </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-lg-4">
-					<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-					<h2 class="homepage-heading">Discover</h2>
+					<a href="recipes"><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"></a>
+					<a href="recipes"><h2 class="homepage-heading">Discover</h2></a>
 					<p class="text-muted">Learn tips and tricks from fellow <br>
 					bakers. Your next challenge awaits you. </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-lg-4">
-					<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-					<h2 class="homepage-heading">Share</h2>
+					<a href="recipes/create"><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"></a>
+					<a href="recipes/create"><h2 class="homepage-heading">Share</h2></a>
 					<p class="text-muted">Write a recipe, then use a recipe. <br>
 					It's the cycle that keeps on giving. </p>
 				</div><!-- /col-lg-4 -->
@@ -47,9 +47,8 @@
 		<div class="container">
 	      <div class="row">
 	        <div class="col-sm-3">
-				{{-- $img = Image::make(Input::file('img/cookies-flower-lemon.jpg'))
-				$img->resize(250, 250);
-				return Response::make($image, 200, ['Content-Type' => 'image/jpg']);
+				{{-- $img = Image::make('img/cookies-flower-lemon.jpg')->resize(250, 250);
+				 return $img->response('jpg');
 				}); --}}
 				<img src="img/cookies-flower-lemon.jpg" alt="Generic Placeholder image" class="img-rounded img-responsive">
 			</div>
