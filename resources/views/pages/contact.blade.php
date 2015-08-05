f9dc4ad629685fbf741401bc0c8c10a08e6a0df8
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-05 23:20:06 +0800

    changed alt text

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -9,7 +9,7 @@
 			<p>Have a question or suggestion about our site? Feel free to email us at <a href="mailto:bakingamigo@gmail.com?subject=Contact Us: Baking Amigo Question/Suggestion">bakingamigo&#64;gmail.com</a> and we'll make sure to get in touch with you as soon as we can.
 			</p>
 			<div class="post_contact_text">
-				<img src="img/small-cupcakes.jpg" alt="Contact Us Adriana Pope" class="img-rounded img-responsive">
+				<img src="img/small-cupcakes.jpg" alt="Cupcakes" class="img-rounded img-responsive">
 			</div>
 		</div>
 	</div>
