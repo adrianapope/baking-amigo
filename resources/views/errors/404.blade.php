25a1deba456e3ceba2d66b2ccbf241af22f64438
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-05 16:38:25 +0800

    added 404 page to errors directory

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+@section('content')
+<div class="404-wrapper">
+	<div class="container">
+		<div class="body">
+			<h3>Oops! Sorry Amigo.
+			The page you've requested could not be found.
+			</h3>
+		</div>
+		<div class="404-photo-wrapper">
+			<div class="body">
+				<img src="img/test.png" alt="Fox" class="img-rounded img-responsive">
+			</div>
+		</div>
+	</div>
+</div>
+
+@stop
