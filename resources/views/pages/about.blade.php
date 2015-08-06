5aba735f3b87c0b6afe20223cc65fc1e6b285683
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-06 22:23:26 +0800

    playing around with design

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,19 +5,23 @@
 <div class="about-wrapper">
 	<div class="container">
 		<h1>About</h1>
-		<div class="body">
-			<blockquote>
-				<p>"Baking Amigo is a space where folks can find and swap thier favorite allergen-free baking recipes."
+		<div class="row">
+			<div class="body">
+				<blockquote>
+					<p>"Baking Amigo is a space where folks can find and swap thier favorite allergen-free baking recipes."
+					</p>
+				</blockquote>
+				<p>!Hola! Welcome to our little community of allergen-free baking enthusiasts where everyone is an amigo. Whether you are just a newb trying out your mitts or an experienced ingredient-substituting expert, this is the place to be.
 				</p>
-			</blockquote>
-			<p>!Hola! Welcome to our little community of allergen-free baking enthusiasts where everyone is an amigo. Whether you are just a newb trying out your mitts or an experienced ingredient-substituting expert, this is the place to be.
-			</p>
-			<p>If you have ever found yourself drooling over a slice of cake or some delicious cookies in a display window, but weren't able to eat it because you are allergic to either gluten, sugar, dairy (or whatever ingredient it may be) ... we know exactly how you feel! Baking Amigo understands how hard it is to satisfy your sweet tooth when you have a specific allergy or food sensitivity.
-			</p>
-			<p>This website was created as a resource for those looking for an awesome dessert recipe without any unwanted ingredients.
-			</p>
-			<div class="post_about_text">
-				<img src="img/chocolate-chip-cookies.jpg" alt="Cupcakes" class="img-rounded img-responsive">
+				<p>If you have ever found yourself drooling over a slice of cake or some delicious cookies in a display window, but weren't able to eat it because you are allergic to either gluten, sugar, dairy (or whatever ingredient it may be) ... we know exactly how you feel! Baking Amigo understands how hard it is to satisfy your sweet tooth when you have a specific allergy or food sensitivity.
+				</p>
+				<p>This website was created as a resource for those looking for an awesome dessert recipe without any unwanted ingredients.
+				</p>
+				<div class="row">
+					<div class="post-about-text">
+						<img src="img/chocolate-chip-cookies.jpg" alt="Cupcakes" class="img-rounded img-responsive">
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
