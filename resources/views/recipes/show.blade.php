ca127e553dc1489d64a310e9bf4dd0af2f3c9664
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-06 22:22:02 +0800

    working on css design of show page (single recipe view)

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -8,23 +8,111 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		<div class="show-index-header">
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 show-index-wrapper">
+			<h1>{{ $recipe->name }}</h1>
 			<div class="row">
-				<div class="col-sm-8">
-					<div class="row">
-						<div class="show-index-image col-sm-6">
-							<img src="http://dummyimage.com/350x350/000/fff" alt= "placeholder image" class="img-rounded img-responsive">
-						</div>
-						<div class="show-index-description col-sm-6">
-							<h1>{{ $recipe->name }} <small>{{ $recipe->created_at->diffForHumans() }}</small></h1>
-							<h4>Description</h4>
-							{{ $recipe->description }}
+				<div class="col-sm-6 col-xs-12">
+					<img src="/img/chocolate-chip-cookies.jpg" class="img-responsive img-rounded">
+				</div>
+				<div class="row">
+					<div class="col-sm-4 col-xs-12 bg-success">
+						<small>Published On:
+							{{ $recipe->published_at }}
+						</small>
+						<hr>
+						<blockquote>
+						<p>{{ $recipe->description }}<p>
+						</blockquote>
+						<div class="col-sm-2 col-xs-12">
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="show-index-body">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 show-index-key-info bg-info">
+			<ul>
+				<li><p>Prep Time:</p>
+						{{ $recipe->prep_time}} {{ $recipe->prep_time_type }}
+				</li>
+				<li><p>Cook Time:</p>
+					{{ $recipe->cook_time }} {{ $recipe->cook_time_type }}
+				</li>
+				<li><p>Ready Time:</p>
+						{{ $recipe->ready_time }} {{ $recipe->ready_time_type }}
+				</li>
+				<li><p>Servings:</p>
+						{{ $recipe->servings }}
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="row">
+				<div class="col-sm-6 col-xs-12 bg-warning">
+					<h3>Ingredients</h3>
+					<p>ingredients go here later dynamically.</p>
+				</div>
+				<div class="row">
+					<div class="col-sm-4 col-xs-12 bg-success">
+						<h3>Instructions</h3>
+						{{ $recipe->instructions }}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="row">
+				<div class="col-sm-6 col-xs-12 bg-danger">
+					<h3>Helpful Notes</h3>
+					{{ $recipe->notes }}
+				</div>
+				<div class="row show-index-tags">
+					<div class="col-sm-4 col-xs-12 bg-info">
+						@unless ($recipe->tags->isEmpty())
+							<h3>Tags</h3>
+							<ul>
+								@foreach ($recipe->tags as $tag)
+									<li><button type="button" class="btn btn-primary btn-xs ">{{ $tag->name }}</button>
+									</li>
+								@endforeach
+							</ul>
+						@endunless
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+							{{--
+
+							<h1>{{ $recipe->name }}</h1>
+
+							<h4>Description</h4>
+							{{ $recipe->description }}
+
+
 								<h4>Instructions</h4>
 								{{ $recipe->instructions }}
 
@@ -54,29 +142,13 @@
 									@endforeach
 								</ul>
 								@endunless
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="sidebar-item-wrapper">
-						<h4>Latest Amigos</h4>
-					</div>
-					<div class="sidebar-item-wrapper">
-						<h4>Recent Recipes</h4>
-					</div>
-					<div class="sidebar-item-wrapper">
-						<h4>New Articles</h4>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
+
 	<div class="page-navigation-wrapper">
 		@if( Auth::user()->id == $recipe->user_id)
 		<a href="/recipes/{{ $recipe->id }}/edit"><span class="btn btn-success">Edit your Recipe</span></a>
 		@endif
 	</div>
-
+ --}}
 @stop
 
