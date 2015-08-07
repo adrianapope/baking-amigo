d4c069288a8e479fd0bb0d4a2d07de64a26320d3
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-07 18:17:17 +0800

    added google font for show page (recipe title)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,16 +7,15 @@
  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Paytone+One' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
 	<meta name="description" content="Recipes for bakers of all skill levels! Join today for free.">
  	@yield('customMeta')
 
  	</head>
 <body>
 	@include('layouts.nav')
-	<div class="container">
         @include('partials.flash')
 		@yield('content')
-	</div>
 
 	<script src="//code.jquery.com/jquery.js"></script>
     <script>
