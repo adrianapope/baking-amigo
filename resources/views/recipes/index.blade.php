1a92c7d4e6901b53e26486e41cc0c927613ef1eb
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-21 19:07:22 +0800

    created a foreach loop in which four recipes display per row on index page

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,22 +1,40 @@
 @extends('app')
 
 @section('content')
-	<div class="container">
-		<div class="recipes-index">
-			<h1>Recipes</h1>
 
-			@foreach ($recipes as $recipe)
-				<div class="recipes-index-single">
-					<h2>
-						<a href="/recipes/{{ $recipe->id }}">{{ ucwords($recipe->name) }}</a>
-					</h2>
-
-					<div class="body">{{ $recipe->description }}</div>
+	<div class="recipes-index-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Recipes</h1>
 				</div>
+			</div>
+
+			@foreach(array_chunk($recipes->all(), 4) as $fourRecipes)
+				<div class="row">
+					@foreach($fourRecipes as $recipe)
+						<div class="col-sm-3">
+							<img src="img/tiny-cookie.jpg" alt="Cookie" class="img-rounded img-responsive">
+							<h2>
+							<a href="/recipes/{{ $recipe->id }}">{{ ucwords($recipe->name) }}</a>
+							</h2>
+
+							<div class="body">{{ $recipe->description }}</div>
+							</div>
+					@endforeach
+				</div>
+				<hr>
 			@endforeach
-		</div>
-		<div class="page-navigation-wrapper">
-			<a href="/recipes/create"><span class="btn btn-success btn-lg">Create a Recipe</span></a>
+
+			<hr>
+
+			<div class="row">
+				<div class="col-sm-12">
+					<a href="/recipes/create"><span class="btn btn-success btn-lg">Create a Recipe</span></a>
+				</div>
+			</div>
 		</div>
 	</div>
+
 @stop
+
