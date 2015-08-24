5708ef775a92a117863dac5fa0b94fe23d860b83
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-24 17:34:49 +0800

    created reviews table

---
+++ database/migrations/2015_08_24_044711_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('reviews', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('recipe_id');
+			$table->integer('user_id');
+			$table->integer('comment_id');
+			$table->integer('rating');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('reviews');
+	}
+
+}
