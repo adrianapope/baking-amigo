fc7e33c06ca7ac7301b2997dda66aa645f042336
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-24 17:36:07 +0800

    updated recipes table to include a float for the calculated score of the review (the star rating)

--- database/migrations/2015_05_24_204417_create_recipes_table.php
+++ database/migrations/2015_05_24_204417_create_recipes_table.php
@@ -20,6 +20,7 @@ class CreateRecipesTable extends Migration {
 			$table->text('description')->nullable();
 			$table->text('instructions');
 			$table->text('notes')->nullable();
+			$table->float('calculated_score');
 			$table->timestamps();
 			$table->timestamp('published_at');
 			$table->integer('prep_time')->nullable();
