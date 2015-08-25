234f1b36b966feda0aa7e1a0f3c11f1641f9e94f
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-25 23:20:37 +0800

    Added class Review (for accessing the model) and Auth (for my middleware). Created logic for getting the recipe, creating the comment, checking if review exists, creating a new review, attaching a review to the comment, saving it, and then redirecting.

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,21 +2,50 @@
 
 use App\Recipe;
 use App\Comment;
+use App\Review;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller {
 
-	public function store(Recipe $recipe, Request $request)
+	public function store($id, Request $request)
 	{
+		//get recipe
+		$recipe = Recipe::whereId($id)->first();
+
+
 		// create the comment
 		$comment = Comment::create([
-			'body' => $request['body'],
+			'body' 		=> $request['body'],
+			'recipe_id' => $recipe->id,
+			'user_id' 	=> Auth::user()->id
 		]);
 
-		return dd($comment);
+
+		// check if review exists
+
+		if( isset($request['rating']) ) {
+
+			// create the new review
+			$review = Review::create([
+				'rating' 		=> $request['rating'],
+				'user_id' 		=> Auth::user()->id,
+				'recipe_id' 	=> $recipe->id,
+				'comment_id' 	=> $comment->id
+			]);
+
+			// attach the review to the comment
+			$comment->review_id = $review->id;
+
+			// save properly to include review_id
+			$comment->save();
+		}
+
+		//return view
+		return redirect("/recipes/$recipe->id");
 	}
 
 
