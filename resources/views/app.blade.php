9bf066661ecc54c4eb3eee3fa26f4b06f73611c5
Adriana Pope <adrianagiancarla@gmail.com>
2015-08-27 13:33:05 +0800

    deleted jQuery script and bootstrap css (cdn version) for now (for star rating later)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,6 @@
 	<link href='http://fonts.googleapis.com/css?family=Paytone+One' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet"/>
 	<meta name="description" content="Recipes for bakers of all skill levels! Join today for free.">
  	@yield('customMeta')
 
@@ -25,8 +24,6 @@
 
 	<script src="{{ elixir('js/app.js') }}"></script>
 	@include('layouts.footer')
-
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
 	<script>
 	$('#myTabs a').click(function (e) {
