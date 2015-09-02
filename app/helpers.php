0509acb045bf897b406d5adedd118cc22034cd1d
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-02 23:06:09 +0800

    created custom helpers file to store function get_stars

---
+++ app/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+// My common functions
+
+function get_stars($avg_rating)
+{
+  if($avg_rating < 1) {
+    return "<i class='fa fa-star-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i>";
+  } elseif($avg_rating >= 1 && $avg_rating < 2) {
+    return "<i class='fa fa-star'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i>";
+  } elseif($avg_rating >= 2 && $avg_rating < 3) {
+    return "<i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i>";
+  } elseif($avg_rating >= 3 && $avg_rating < 4) {
+    return "<i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i>";
+  } elseif($avg_rating >= 4 && $avg_rating < 5) {
+    return "<i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star-o'></i>";
+  } else {
+    return "<i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i>";
+  }
+}
