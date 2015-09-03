9da24192599a755df078d18aac913aabc6345f7e
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-03 18:03:03 +0800

    added contents from list.blade.php into edit.blade.php which handles the error messages

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,4 +1,18 @@
 
+@if (count($errors) > 0)
+	<div class="alert alert-danger alert-important alert-permanent alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
+
+
 @if (Session::has('flash_message'))
 	<div class="alert alert-success {{ Session::has('flash_message_important') ? 'alert-important' : '' }}">
 		@if (Session::has('flash_message_important'))
@@ -9,4 +23,12 @@
 	</div>
 @endif
 
-
+@if (Session::has('flash_message_permanent'))
+	<div class="alert alert-success alert-permanent {{ Session::has('flash_message_important') ? 'alert-important' : '' }}">
+		@if (Session::has('flash_message_important'))
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span></button>
+		@endif
+		{{ Session::get('flash_message_permanent') }}
+	</div>
+@endif
