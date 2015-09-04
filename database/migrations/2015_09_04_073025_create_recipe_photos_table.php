7e1316642dd91760783647b4794abff62d3698dc
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-04 22:25:47 +0800

    changed migration name to recipe_photos_table (as I may have more than one photo related table and need to differentiate). Added the schema. And changed 'photo' to path

---
+++ database/migrations/2015_09_04_073025_create_recipe_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipePhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('photo_id')->references('id')->on('recipes')->onDelete('cascade');
+			$table->string('path');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photos');
+	}
+
+}
