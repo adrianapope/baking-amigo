f025b0f97ca894676842a4c4b42c457c255d3288
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-09 00:31:17 +0800

    new flash.blade.php file updated to contain sweet alert functionality to check for 'flash_message' key or 'flash_message_overlay' key

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,19 +1,32 @@
 
-@if (count($errors) > 0)
-	<div class="alert alert-danger alert-important alert-permanent alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-		<ul>
-			@foreach ($errors->all() as $error)
-				<li>{{ $error }}</li>
-			@endforeach
-		</ul>
-	</div>
+@if (session()->has('flash_message'))
+
+	<script>
+		swal({
+			title: "{{ session('flash_message.title') }}",
+			text: "{{ session('flash_message.message') }}",
+			type: "{{ session('flash_message.level') }}",
+			timer: 1700,
+			showConfirmButton: false
+		});
+	</script>
+@endif
+
+@if (session()->has('flash_message_overlay'))
+
+	<script>
+		swal({
+			title: "{{ session('flash_message_overlay.title') }}",
+			text: "{{ session('flash_message_overlay.message') }}",
+			type: "{{ session('flash_message_overlay.level') }}",
+			ConfirmButtonText: 'Okay'
+		});
+	</script>
 @endif
 
 
-@if (Session::has('flash_message'))
+
+{{-- @if (Session::has('flash_message'))
 	<div class="alert alert-success {{ Session::has('flash_message_important') ? 'alert-important' : '' }}">
 		@if (Session::has('flash_message_important'))
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -32,3 +45,4 @@
 		{{ Session::get('flash_message_permanent') }}
 	</div>
 @endif
+ --}}
