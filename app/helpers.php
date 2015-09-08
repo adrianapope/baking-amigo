bd53d1f518ea5c1ab72578521c29de4526eeabb5
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-09 00:21:56 +0800

    added new helper function flash() which will work with sweet alert. grabs an instance of flash out of the service container. will default to info unless otherwise stated.

--- app/helpers.php
+++ app/helpers.php
@@ -39,3 +39,14 @@ function get_stars($avg_rating)
   }
 
 }
+
+ function flash($title = null, $message = null)
+  {
+    $flash = app('App\Http\Flash');
+
+    if (func_num_args() == 0) {
+      return $flash;
+    }
+
+    return $flash->info($title, $message);
+  }
