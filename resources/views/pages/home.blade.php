62d0df95337b482592826f289b42fa0c3aad3ffb
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-22 00:14:13 +0800

    put slash before img path

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -29,19 +29,19 @@
     	<div class="container">
 			<div class="row">
 				<div class="col-sm-4">
-					<a href="auth/register"><img class="img-circle" src="img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
+					<a href="auth/register"><img class="img-circle" src="/img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
 					<a href="auth/register"><h2 class="homepage-heading">Join</h2></a>
 					<p class="text-muted">Want to bake something amazing that <br>
 					impresses heaps of folks? Sign up today. </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-sm-4">
-					<a href="recipes"><img class="img-circle" src="img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
-					<a href="recipes"><h2 class="homepage-heading">Discover</h2></a>
+					<a href="/recipes/index"><img class="img-circle" src="/img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
+					<a href="/recipes/index"><h2 class="homepage-heading">Discover</h2></a>
 					<p class="text-muted">Learn tips and tricks from fellow <br>
 					bakers. Your next challenge awaits you. </p>
 				</div><!-- /col-lg-4 -->
 				<div class="col-sm-4">
-					<a href="recipes/create"><img class="img-circle" src="img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
+					<a href="recipes/create"><img class="img-circle" src="/img/tiny-chip.png" alt="Chocolate Chip Cookie" width="140" height="140"></a>
 					<a href="recipes/create"><h2 class="homepage-heading">Share</h2></a>
 					<p class="text-muted">Write a recipe, then use a recipe. <br>
 					It's the cycle that keeps on giving. </p>
@@ -58,19 +58,19 @@
 	      	<h2>Explore Recipes by Category</h2>
 				<div class="row">
 	        		<div class="col-xs-6 col-sm-3">
-						<img src="img/tiny-cookie.jpg" alt="Cookie" class="img-rounded img-responsive">
+						<img src="/img/tiny-cookie.jpg" alt="Cookie" class="img-rounded img-responsive">
 						<h3>Cookies</h3>
 					</div>
 					<div class="col-xs-6 col-sm-3">
-						<img src="img/tiny-bread.jpg" alt="Bread" class="img-rounded img-responsive">
+						<img src="/img/tiny-bread.jpg" alt="Bread" class="img-rounded img-responsive">
 						<h3>Breads</h3>
 					</div>
 			  		<div class="col-xs-6 col-sm-3">
-						<img src="img/tiny-cupcakes.jpg" alt="Cupcakes" class="img-rounded img-responsive">
+						<img src="/img/tiny-cupcakes.jpg" alt="Cupcakes" class="img-rounded img-responsive">
 						<h3>Cupcakes</h3>
 					</div>
 				    <div class="col-xs-6 col-sm-3">
-						<img src="img/tiny-handpies.jpg" alt="Handpies" class="img-rounded img-responsive">
+						<img src="/img/tiny-handpies.jpg" alt="Handpies" class="img-rounded img-responsive">
 						<h3>Pies</h3>
 					</div>
 					</div>
@@ -80,19 +80,19 @@
 		<div class="container">
 	      <div class="row">
 	        <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-donuts.jpg" alt="Donuts" class="img-rounded img-responsive">
+				<img src="/img/tiny-donuts.jpg" alt="Donuts" class="img-rounded img-responsive">
 				<h3>Donuts</h3>
 			</div>
 			  <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-cake.jpg" alt="Cake" class="img-rounded img-responsive">
+				<img src="/img/tiny-cake.jpg" alt="Cake" class="img-rounded img-responsive">
 				<h3>Cakes</h3>
 			</div>
 			  <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-brownie.jpg" alt="Brownie" class="img-rounded img-responsive">
+				<img src="/img/tiny-brownie.jpg" alt="Brownie" class="img-rounded img-responsive">
 				<h3>Brownies</h3>
 			</div>
 			  <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-raw.jpg" alt="Raw" class="img-rounded img-responsive">
+				<img src="/img/tiny-raw.jpg" alt="Raw" class="img-rounded img-responsive">
 				<h3>Raw</h3>
 
 				</div>
@@ -103,20 +103,20 @@
 		<div class="container">
 	      <div class="row">
 			  <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-muffin.jpg" alt="Muffin" class="img-rounded img-responsive">
+				<img src="/img/tiny-muffin.jpg" alt="Muffin" class="img-rounded img-responsive">
 			    <h3>Muffins</h3>
 			</div>
 			  <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-bars.jpg" alt="Bars" class="img-rounded img-responsive">
+				<img src="/img/tiny-bars.jpg" alt="Bars" class="img-rounded img-responsive">
 				<h3>Bars</h3>
 
 			</div>
 			  <div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-tea.jpg" alt="Tea" class="img-rounded img-responsive">
+				<img src="/img/tiny-tea.jpg" alt="Tea" class="img-rounded img-responsive">
 				<h3>Tea Time</h3>
 				</div>
 				<div class="col-xs-6 col-sm-3">
-				<img src="img/tiny-macaroons.jpg" alt="Macaroons" class="img-rounded img-responsive">
+				<img src="/img/tiny-macaroons.jpg" alt="Macaroons" class="img-rounded img-responsive">
 				<h3>Ethnic</h3>
 			</div>
 			</div>
