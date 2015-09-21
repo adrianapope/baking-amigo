21e36c9b5c4cf9e105220c70d0eb4d0b501eb132
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-22 00:15:55 +0800

    added Dropzone stylesheet and script with custom options

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
 	<title>@yield('customTitle', 'Baking Amigo')</title>
  	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css">
+ 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/dropzone.css">
 	<link href='http://fonts.googleapis.com/css?family=Paytone+One' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
@@ -36,5 +37,14 @@
 
 	@include('partials.flash')
 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/dropzone.js">
+	</script>
+	<script>
+		Dropzone.options.addPhotosForm = {
+			paramName: 'photo', // it was defaulted to 'file'
+			maxFilesize: 10,
+			acceptedFiles: 'jpg, .jpeg, .png, .bmp'
+		};
+	</script>
 </body>
 </html>
